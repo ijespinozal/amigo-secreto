c608055e51b0f22b2fa1aae5b39be52c5689f01e
ijespinozal <israelespinoza1298@hotmail.com>
2025-11-15 10:16:46 -0500

    Se actualiza credenciales

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,9 +8,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
     dialect: "mysql",
-    logging: false,
+    port: process.env.DB_PORT
   }
 );
 
